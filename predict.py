0ee11ca9a4bb8de9b604d446ecba9e23a41a2f45
WiktorSzajerka <wszajerka@wp.pl>
2026-05-04 19:02:00 +0200

    .

--- predict.py
+++ predict.py
@@ -1,11 +1,21 @@
 import numpy as np
+import librosa
 from joblib import load
 
 with open("rocket1.pkl", "rb") as f:
     clf = load(f)
 
+filepath = "path"
+max_duration = 7.5
+freq = 16000
+max_size = int(max_duration*freq)
 
-data = np.load("raw_signal.npy")
-print(data[1])
-y_pred = clf.predict(data[1:2,:])
+y, sr = librosa.load(filepath, sr=freq)
+if y.size > max_size:
+    y = y[:max_size]
+elif y.size < max_size:
+    y = np.pad(y, (0, max_size - len(y)))
+
+
+y_pred = clf.predict(y)
 print(y_pred)
